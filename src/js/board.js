9d58a47f7202471c609d872dbade28c2d7e16ba4
fredsjr <Oli4schell@gmail.com>
2023-06-21 12:56:24 +0200

    grid complete with movable character

--- src/js/board.js
+++ src/js/board.js
@@ -34,41 +34,96 @@ export class Board extends Scene{
         let board = SpriteSheet.fromImageSource({
             image: Resources.Board,
             grid: {
-                rows: 8,
-                columns: 8,
+                rows: 10,
+                columns: 11,
                 spriteHeight: 52,
                 spriteWidth: 52
+
             }
         });
 
-        // hier wordt de tilemap aangemaakt
-        let tileMap = new TileMap({
-            rows: 8,
-            columns: 8,
-            tileHeight: 52,
-            tileWidth: 52
-        });
-
-        for (let cell of tileMap.tiles) {
-            const sprite = board.getSprite(0,0);
-            if (sprite) {
-                cell.addGraphic(sprite)
+        for (let x = 0; x <= 9; x ++) {
+            for (let y = 0; y <= 10; y ++) {
+                let tile = new Actor({
+                    pos: new Vector(x * 55, y * 55),
+                    anchor: new Vector(0, 0),
+                    width: 55,
+                    height: 55
+                })
+                // tile.anchor = new Vector(0.5,0.5)
+                // tile.anchor.setTo(-1, -1)
+                this.add(tile)
             }
         }
 
-}
-
-}
-
-import { Actor } from "excalibur";
-import { Resources } from "./resources.js";
-
-export class Board extends Actor {
-    constructor() {
-        super({
-            width: Resources.Board.width,
-            height: Resources.Board.height,
+        // Create the player actor
+        this.player = new Actor({
+            pos: new Vector(0, 0), // Initial position
+            anchor: Vector.Zero,
+            width: 55,
+            height: 55,
+            color: Color.Green // You can change the color as needed
         });
+
+        this.add(this.player);
+
+        // Set the initial tile for the player
+        this.currentTile = { x: 0, y: 0 };
+
+        // Attach event listeners to handle button presses
+        window.addEventListener("keydown", this.handleKeyPress.bind(this));
     }
+
+    handleKeyPress(event) {
+        // Handle arrow key presses
+        switch (event.key) {
+            case "ArrowUp":
+                this.movePlayerToTile(this.currentTile.x, this.currentTile.y - 1);
+                break;
+            case "ArrowDown":
+                this.movePlayerToTile(this.currentTile.x, this.currentTile.y + 1);
+                break;
+            case "ArrowLeft":
+                this.movePlayerToTile(this.currentTile.x - 1, this.currentTile.y);
+                break;
+            case "ArrowRight":
+                this.movePlayerToTile(this.currentTile.x + 1, this.currentTile.y);
+                break;
+            default:
+                break;
+        }
+    }
+
+    movePlayerToTile(newX, newY) {
+        // Check if the new tile is within the board boundaries
+        if (newX >= 0 && newX < 10 && newY >= 0 && newY < 11) {
+            // Update the player's position
+            this.player.pos = new Vector(newX * 55, newY * 55);
+            this.currentTile.x = newX;
+            this.currentTile.y = newY;
+        }
+    }
+
+
+
+        // // hier wordt de tilemap aangemaakt
+        // let tileMap = new TileMap({
+        //     rows: 10,
+        //     columns: 11,
+        //     tileHeight: 52,
+        //     tileWidth: 52
+        // });
+        //
+        // for (let cell of tileMap.tiles) {
+        //     const sprite = board.getSprite(0,0);
+        //     if (sprite) {
+        //         cell.addGraphic(sprite)
+        //
+        //
+        //
+        //      }
+        //
+        // }
+
 }
 
--- src/js/game.js
+++ src/js/game.js
@@ -3,21 +3,22 @@ import { Actor, Engine, Vector, Scene } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
 import {StartScreen} from "./startscreen.js";
 import { BoardScreen } from './boardgame.js'
+import {Board} from "./board.js";
 
 export class Game extends Engine {
 
     constructor() {
         super({ width: 1200, height: 900 })
         this.start(ResourceLoader).then(() => this.startGame())
-        this.showDebug(true)
-        this.debug.transform.showAll = true
+        // this.showDebug(true)
+        // this.debug.transform.showAll = true
     }
 
     startGame() {
         console.log("start de game!")
         this.addScene('startScreen', new StartScreen())
-        this.addScene('boardScreen', new BoardScreen())
-        this.goToScene('startScreen')
+        this.addScene('boardScreen', new Board())
+        this.goToScene('boardScreen')
     }
 }
 
